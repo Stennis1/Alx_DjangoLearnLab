e8ea8e2d7c2e18a86c32fd795acff6e03c85022b
Stennis1 <stennismith@gmail.com>
2025-02-19 14:46:07 +0000

    Update query_samples.py script

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,14 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Particular author 
+# Particular author
+author_name = "J.K. Rowling" 
+author= Author.objects.get(name=author_name)
 books_by_author = author.books.all()
 
 
 # List all books in a Library 
+library_name = "Slytherin"
+library = Library.objects.all(name=library_name)
 books_in_library = library.books.all()
 
 
