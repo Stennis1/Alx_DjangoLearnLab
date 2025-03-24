4b55a0fbd5e9b70e51f41f2c53472eb48ca913a8
Stennis1 <stennismith@gmail.com>
2025-03-24 12:36:11 +0000

    Create django_blog project structure.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,3 +15,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/list_books.html'
     context_object_name = "library"
+    
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3ow_s9bik&m7b1#kbv7&=%-d7xa!#ote_-16dgff2q)+&@04^'
+SECRET_KEY = 'django-insecure-5m$o@$(@q3@+$=3m%w7goj0%iyit8w)(4^$wsg!wswfyo90x&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'stennis',
+        'PASSWORD': 'stennis12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
@@ -116,16 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-]
-
-TEMPLATES = [
-    {
-        'DIRS': [BASE_DIR / 'templates']
-    }
 ]
 
 # Default primary key field type
