495bab7797c6f4d28c499906cf46a818a9bfba79
Stennis1 <stennismith@gmail.com>
2025-03-26 09:57:24 +0000

    Add blog templates and views.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('accounts/logout/', views.logout_view, name='logout'),
     path('accounts/register/', views.register, name='register'),
     path('accounts/profile/', views.profile, name='profile'),
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CustomUserCreateForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import CustomUserCreateForm, PostForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 # View to handle registration and login
@@ -16,7 +16,7 @@ def register(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Account created successsfully, you can log in now.")
-            return redirect('login')
+            return redirect('post-list')
         
     else:
         form = CustomUserCreateForm()
@@ -24,14 +24,12 @@ def register(request):
 
 
 # Profile required
-@login_required 
 def profile(request):
     if request.method == 'POST':
         user = request.user
         user.username = request.POST.get('username')
         user.email = request.POST.get('email')
         user.save()
-        messages.success(request, 'Profile updated successfully')
         return redirect('profile')
     else:
         return render(request, 'blog/profile.html', {'user': request.user})
@@ -42,3 +40,44 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have logged out successfully')
     return redirect('login')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post 
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post 
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  #only author can edit
+    
+class PostDeleteView(DeleteView):
+    model = Post 
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return super().request.user == post.author
